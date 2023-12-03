797e22da6c7f0f4317f9c7da702fe928b5062c59
Hoimaly <88880664+hoimaly@users.noreply.github.com>
2023-12-03 17:42:07 +0700

    Function multi.py has been upload

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from sub import *
+from multi import *
 
 def main():
     print("Hello World!")
     su = sub(5, 3)
     print(f'Sub = {su}')
+    printf(f'Multi = {multi(5, 3)}')
 
 if __name__ == "__main__":
     main()

b579050d2a639e91650a3b0aca9cce983fa449f6
Hoimaly <88880664+hoimaly@users.noreply.github.com>
2023-12-03 18:21:12 +0700

    Updating new function div.py and modifying test.py again

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from sub import *
+from div import *
 
 def main():
     print("Hello World!")
     su = sub(5, 3)
     print(f'Sub = {su}')
+    print(f'Div = {div(5, 3)}')
 
 if __name__ == "__main__":
     main()
